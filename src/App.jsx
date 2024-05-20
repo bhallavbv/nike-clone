f3716f025454934c6cc1dd715850dc497a308f87
Vaibhav Bhalla <bhallavbv@gmail.com>
2024-05-20 16:47:14 +0530

    Create base page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
-const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-};
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+    <section className="padding">Popular products</section>
+    <section className="padding">Super quality</section>
+    <section className="padding-x py-10">Services</section>
+    <section className="padding">Special offers</section>
+    <section className="padding bg-pale-blue">Customer reviews</section>
+    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+    <section className="bg-black padding-x padding-t pb-8">Footer</section>
+  </main>
+);
 
 export default App;
